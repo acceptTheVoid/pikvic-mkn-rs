a5d3c7925183805a789da95780af6f5270b6463b
acceptTheVoid <arcad2004@gmail.com>
2022-06-06 15:03:25 +1000

    Седьмой (вроде бы) урок Загуменова....

--- src/main.rs
+++ src/main.rs
@@ -3,80 +3,86 @@ extern crate rocket;
 #[macro_use]
 extern crate rocket_dyn_templates;
 
-use std::sync::Mutex;
-use rocket::{serde::Deserialize, http::Status, response::Redirect};
+use std::{fs, sync::Mutex};
+
+use rocket::serde::{Deserialize, Serialize, json::Json};
 use rocket_dyn_templates::Template;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Clone)]
 #[serde(crate = "rocket::serde")]
-struct Item {
-    name: String,
-    price: usize,
+struct News {
+    id: usize,
+    title: String,
+    content: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct NewNews<'a> {
+    title: &'a str,
+    content: &'a str,
 }
 
 struct State {
-    items: Mutex<Vec<Item>>,
-    cart: Mutex<Vec<Item>>,
+    news: Mutex<Vec<News>>,
 }
 
 #[get("/")]
-fn index(state: &rocket::State<State>) -> Template {
-    let items = &*state.items.lock().unwrap();
-    let cart = &*state.cart.lock().unwrap();
-    let ctx = context! { items, cart };
-
-    Template::render("index", ctx)
+fn index(news: &rocket::State<State>) -> Template {
+    let news = &*news.news.lock().unwrap();
+    Template::render("index", context!{ news })
 }
 
-#[get("/<id>")]
-fn get_item(id: usize, state: &rocket::State<State>) -> Result<Template, Status> {
-    let items = &*state.items.lock().unwrap();
-    if items.len() <= id {
-        return Err(Status::NotFound) 
-    }
-
-   Ok(Template::render("item", context!{ item: items[id].clone(), id }))
+#[get("/news/<id>")]
+fn get_content(id: usize, news: &rocket::State<State>) -> Json<News> {
+    let news = &*news.news.lock().unwrap();
+    Json(news[id - 1].clone())
 }
 
-#[get("/cart/add?<id>")]
-fn cart_add(id: Option<usize>, state: &rocket::State<State>) -> Redirect {
-    let items = &*state.items.lock().unwrap();
-    if let Some(id) = id {
-        if id < items.len() {    
-            let cart = &mut *state.cart.lock().unwrap();
-            cart.push(items[id].clone());
-        }
-    }
+#[post("/news", data = "<data>")]
+fn post_news(data: Json<NewNews<'_>>, news: &rocket::State<State>) -> Json<News> {
+    let news = &mut *news.news.lock().unwrap();
+    let data = News {
+        id: news.len() + 1,
+        title: data.title.to_string(),
+        content: data.content.to_string(),
+    };
 
-    Redirect::to(uri!("/"))
-}
-
-#[get("/cart/remove?<id>")]
-fn cart_remove(id: Option<usize>, state: &rocket::State<State>) -> Redirect {
-    if let Some(id) = id {
-        let cart = &mut *state.cart.lock().unwrap();
-        if id < cart.len() {
-            cart.remove(id);
-        }
-    }
-
-    Redirect::to(uri!("/"))
+    news.push(data.clone());
+    Json(data)
 }
 
 #[launch]
 fn rocket() -> _ {
-    let state = State { 
-        items: Mutex::new(vec![
-            Item {name: String::from("asdfjasd"), price: 324},
-            Item {name: String::from("asdsdsdad"), price: 3224},
-            Item {name: String::from("asASddsaasd"), price: 3124},
-            Item {name: String::from("4234232432asASddsaasd"), price: 3124}
-        ]),
-        cart: Mutex::new(vec![])
-    };
+    let news = get_news().unwrap();
+    
+    println!("{:#?}", news);
+
+    let news = Mutex::new(news);
+    let state = State { news };
 
     rocket::build()
-        .mount("/", routes![index, get_item, cart_add, cart_remove])
+        .mount("/", routes![index, get_content, post_news])
         .manage(state)
         .attach(Template::fairing())
+}
+
+fn get_news() -> Result<Vec<News>, std::io::Error> {
+    let contents = fs::read_to_string("news.txt")?;
+    let mut lines = contents.lines();
+
+    let mut news = vec![];
+    if let Some(n) = lines.next() {
+        let n = n.parse::<usize>().unwrap();
+        for _ in 0..n {
+            let id = lines.next().unwrap().parse::<usize>().unwrap();
+            let title = lines.next().unwrap().to_string();
+            let content = lines.next().unwrap().to_string();
+            let item = News { id, title, content };
+
+            news.push(item);
+        }
+    }
+
+    Ok(news)
 }
